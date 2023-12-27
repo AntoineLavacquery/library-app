f212794f8d222bea8635021cb916c5112083a726
Antoine LAVACQUERY <antoine.lavacquery@proton.me>
2023-12-27 19:50:51 +0100

    Add return from modal

--- script.js
+++ script.js
@@ -11,14 +11,52 @@ function addBookToLibrary() {
     // do things
 }
 
-const dialog = document.querySelector("dialog");
-const addButton = document.querySelector("button.addMenu");
-const closeButton = document.querySelector("button.close");
+const addDialog = document.querySelector("dialog");
+const addMenuButton = document.querySelector("button.addMenu");
+const confirmAddButton = document.querySelector("button.add");
+// const closeButton = document.querySelector("button.close");
+const titleInput = document.querySelector("input[name=title]");
 
-addButton.addEventListener("click", () => {
-  dialog.showModal();
+const pageTitle = document.querySelector("h1");
+
+addMenuButton.addEventListener("click", () => {
+    addDialog.showModal();
 });
 
-closeButton.addEventListener("click", () => {
-  dialog.close();
+confirmAddButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const chosenTitle = addDialog.close(titleInput.value);
 });
+
+
+addDialog.addEventListener("close", (e) => {
+    pageTitle.innerText =
+    addDialog.returnValue === "default"
+        ? "Pas de valeur retournée."
+        : `${addDialog.returnValue}`;
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// cpassInput.addEventListener('input', function () {
+//     const passValue = passInput.value;
+//     const cpassValue = this.value;
+
+//     if (passValue !== cpassValue) {
+//         passInput.setCustomValidity("Passwords don't match");
+//         this.setCustomValidity("Passwords don't match");
+//     } else {
+//         passInput.setCustomValidity("");
+//         this.setCustomValidity("");
+//     }
+// });

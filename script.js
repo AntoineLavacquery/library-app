f19cacd4ed8dc96d71a28c0b328088e233c2d9ab
Antoine LAVACQUERY <antoine.lavacquery@proton.me>
2023-12-28 12:59:40 +0100

    Add append to myLibrary array, display from it

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const booksGrid = document.querySelector("main.books-grid");
+
+const addDialog = document.querySelector("dialog");
+const addMenuButton = document.querySelector("button.addMenu");
+const confirmAddButton = document.querySelector("button.add");
+// const closeButton = document.querySelector("button.close");
+
+const titleInput = document.querySelector("input[name=title]");
+const authorInput = document.querySelector("input[name=author]");
+const pagesInput = document.querySelector("input[name=pages]");
+const readCheckbox = document.querySelector("input[name=read]");
+
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -7,17 +20,24 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary() {
-    // do things
+const firstBook = new Book(
+    "Terre des hommes",
+    "Antoine de Saint-Exupéry",
+    224
+)
+
+booksGrid.innerHTML = "";
+booksGrid.appendChild(generateBookCard("Wind, Sand and Stars", "Antoine de Saint-Exupéry", 224));
+
+function displayBooks() {
+    booksGrid.innerHTML = "";
+    booksGrid.appendChild(generateBookCard("Wind, Sand and Stars", "Antoine de Saint-Exupéry", 224));
+
+    for (const bookData of myLibrary) {
+        booksGrid.appendChild(generateBookCard(bookData.title, bookData.author, bookData.pages));
+    }
 }
 
-const addDialog = document.querySelector("dialog");
-const addMenuButton = document.querySelector("button.addMenu");
-const confirmAddButton = document.querySelector("button.add");
-// const closeButton = document.querySelector("button.close");
-const titleInput = document.querySelector("input[name=title]");
-
-const pageTitle = document.querySelector("h1");
 
 addMenuButton.addEventListener("click", () => {
     addDialog.showModal();
@@ -25,38 +45,66 @@ addMenuButton.addEventListener("click", () => {
 
 confirmAddButton.addEventListener("click", (event) => {
     event.preventDefault();
-    const chosenTitle = addDialog.close(titleInput.value);
+    const bookData = new Book(
+        titleInput.value,
+        authorInput.value,
+        pagesInput.value,
+        readCheckbox.checked
+    );
+    addDialog.close(JSON.stringify(bookData));
 });
 
-
 addDialog.addEventListener("close", (e) => {
-    pageTitle.innerText =
-    addDialog.returnValue === "default"
-        ? "Pas de valeur retournée."
-        : `${addDialog.returnValue}`;
-  });
+    console.log(addDialog.returnValue);
 
+    try {
+        const bookData = JSON.parse(addDialog.returnValue);
 
+        if (bookData && typeof bookData === "object") {
+            myLibrary.push(bookData);
+            // console.log(bookData.title);
+            // console.log(bookData.author);
+            // console.log(bookData.pages);
+            // generateBookCard(bookData.title, bookData.author, bookData.pages);
+        }
+    } catch (error) {
+        console.error("Error when analysing JSON :", error);
+    }
 
+    displayBooks();
+});
 
+function generateBookCard(title, author, pages) {
+    console.log(title, typeof author, typeof pages);
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("card");
 
+    const titleSpan = document.createElement("span");
+    titleSpan.classList.add("title");
+    titleSpan.innerText = title;
 
+    const authorSpan = document.createElement("span");
+    authorSpan.classList.add("author");
+    authorSpan.innerText = author;
 
+    const pagesSpan = document.createElement("span");
+    pagesSpan.classList.add("pages");
+    pagesSpan.innerText = pages;
 
+    const readButton = document.createElement("button");
+    readButton.classList.add("read");
+    readButton.innerText = "Read";
 
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("del");
+    deleteButton.innerText = "Delete";
 
+    // Ajouter les éléments au div de la carte
+    bookCard.appendChild(titleSpan);
+    bookCard.appendChild(authorSpan);
+    bookCard.appendChild(pagesSpan);
+    bookCard.appendChild(readButton);
+    bookCard.appendChild(deleteButton);
 
-
-
-// cpassInput.addEventListener('input', function () {
-//     const passValue = passInput.value;
-//     const cpassValue = this.value;
-
-//     if (passValue !== cpassValue) {
-//         passInput.setCustomValidity("Passwords don't match");
-//         this.setCustomValidity("Passwords don't match");
-//     } else {
-//         passInput.setCustomValidity("");
-//         this.setCustomValidity("");
-//     }
-// });
+    return bookCard;
+}

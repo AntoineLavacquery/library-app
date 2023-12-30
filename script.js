c66904785cce5a841092932335b92ceac9a1a2e5
Antoine LAVACQUERY <antoine.lavacquery@proton.me>
2023-12-30 01:03:59 +0100

    Add functionnality to isRead checkbox on cards, fix delete button

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const confirmAddButton = document.querySelector("button.add");
 const titleInput = document.querySelector("input[name=title]");
 const authorInput = document.querySelector("input[name=author]");
 const pagesInput = document.querySelector("input[name=pages]");
-const isReadCheckbox = document.querySelector("input[name=isRead]");
+const isReadAddCheckbox = document.querySelector("input[name=isRead]");
 const delButton = document.querySelector("button.del");
 
 // Data Structures
@@ -38,22 +38,53 @@ class Library {
     }
 
     addBook(newBook) {
-        this.books.push(newBook);
+        if (!this.books.includes(newBook)) {
+            this.books.push(newBook);
+        } else {
+            alert("The book is already present");
+        }
     }
 
-    removeBook(bookTitle) {
-        this.books = this.books.filter((book) => book.title !== bookTitle)
+    addBook(newBook) {
+        const bookExists = this.books.some(
+            (book) =>
+                book.title === newBook.title && book.author === newBook.author
+        );
+
+        if (!bookExists) {
+            this.books.push(newBook);
+        } else {
+            alert("The book is already present");
+        }
+    }
+
+    updateIsRead(bookTitle, bookAuthor, bookPages, bookIsRead) {
+        const foundBook = this.books.find(
+            (book) =>
+                book.title === bookTitle ||
+                book.author === bookAuthor ||
+                book.pages === bookPages ||
+                book.isRead === bookIsRead
+        );
+
+        foundBook.isRead = !foundBook.isRead;
+    }
+
+    removeBook(bookTitle, bookAuthor, bookPages, bookIsRead) {
+        console.log(bookIsRead);
+        for (let book of this.books) {
+            console.log(book.isRead);
+            console.log(book.isRead !== bookIsRead);
+        }
+        this.books = this.books.filter(
+            (book) =>
+                book.title !== bookTitle ||
+                book.author !== bookAuthor ||
+                book.pages !== bookPages ||
+                book.isRead !== bookIsRead
+        );
     }
 }
-
-// const myLibrary = [
-//     {
-//         title: "Wind, Sand and Stars",
-//         author: "Antoine de Saint-Exupéry",
-//         pages: "224",
-//         isRead: true,
-//     },
-// ];
 
 const myLibrary = new Library();
 displayBooks();
@@ -64,7 +95,12 @@ function displayBooks() {
     booksGrid.innerHTML = "";
     for (const bookData of myLibrary.books) {
         booksGrid.appendChild(
-            generateBookCard(bookData.title, bookData.author, bookData.pages)
+            generateBookCard(
+                bookData.title,
+                bookData.author,
+                bookData.pages,
+                bookData.isRead
+            )
         );
     }
 }
@@ -79,7 +115,7 @@ confirmAddButton.addEventListener("click", (event) => {
         titleInput.value.trim() || undefined,
         authorInput.value.trim() || undefined,
         pagesInput.value.trim() || undefined,
-        isReadCheckbox.checked
+        isReadAddCheckbox.checked
     );
     addDialog.close(JSON.stringify(bookData));
 });
@@ -100,8 +136,7 @@ addDialog.addEventListener("close", (e) => {
     displayBooks();
 });
 
-function generateBookCard(title, author, pages) {
-    console.log(title, typeof author, typeof pages);
+function generateBookCard(title, author, pages, isRead) {
     const bookCard = document.createElement("div");
     bookCard.classList.add("card");
 
@@ -115,11 +150,23 @@ function generateBookCard(title, author, pages) {
 
     const pagesSpan = document.createElement("span");
     pagesSpan.classList.add("pages");
-    pagesSpan.innerText = pages;
+    pagesSpan.innerText = `${pages} pages`;
 
-    const isReadButton = document.createElement("button");
-    isReadButton.classList.add("isRead");
-    isReadButton.innerText = "Read";
+    const isReadContainer = document.createElement("div");
+    isReadContainer.classList.add("isRead")
+
+    const isReadCardLabel = document.createElement("label");
+    isReadCardLabel.for = "isRead";
+    isReadCardLabel.innerText = "Read";
+
+    const isReadCardCheckbox = document.createElement("input");
+    isReadCardCheckbox.name = "isRead";
+    isReadCardCheckbox.type = "checkbox";
+    isReadCardCheckbox.classList.add("isRead");
+    isReadCardCheckbox.checked = isRead;
+
+    isReadContainer.appendChild(isReadCardLabel);
+    isReadContainer.appendChild(isReadCardCheckbox);
 
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("del");
@@ -128,7 +175,7 @@ function generateBookCard(title, author, pages) {
     bookCard.appendChild(titleSpan);
     bookCard.appendChild(authorSpan);
     bookCard.appendChild(pagesSpan);
-    bookCard.appendChild(isReadButton);
+    bookCard.appendChild(isReadContainer);
     bookCard.appendChild(deleteButton);
 
     return bookCard;
@@ -136,12 +183,36 @@ function generateBookCard(title, author, pages) {
 
 booksGrid.addEventListener("click", function (event) {
     if (event.target.classList.contains("del")) {
-        var card = event.target.closest(".card");
+        const card = event.target.closest(".card");
 
         if (card) {
-            var bookTitle = card.querySelector(".title").textContent;
+            const bookTitle = card.querySelector("span.title").textContent;
+            const bookAuthor = card.querySelector("span.author").textContent;
+            const bookPages = card.querySelector("span.pages").textContent;
+            const bookIsRead = card.querySelector("input.isRead").checked;
 
-            myLibrary.removeBook(bookTitle);
+            myLibrary.removeBook(bookTitle, bookAuthor, bookPages, bookIsRead);
+            displayBooks();
+        }
+    }
+});
+
+booksGrid.addEventListener("click", function (event) {
+    if (event.target.classList.contains("isRead")) {
+        const card = event.target.closest(".card");
+
+        if (card) {
+            const bookTitle = card.querySelector("span.title").textContent;
+            const bookAuthor = card.querySelector("span.author").textContent;
+            const bookPages = card.querySelector("span.pages").textContent;
+            const bookIsRead = card.querySelector("input.isRead").checked;
+
+            myLibrary.updateIsRead(
+                bookTitle,
+                bookAuthor,
+                bookPages,
+                bookIsRead
+            );
             displayBooks();
         }
     }
